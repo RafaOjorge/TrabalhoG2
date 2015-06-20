a383fd8d826e0712537ef61171d528f8b5387053
rafaelOjorge <rafael.joliveira@hotmail.com>
2015-06-20 12:58:34 -0300

    classes adicionadas

---
+++ AluguelQuadras/AluguelQuadras/Models/Quadras.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AluguelQuadras.Models
+{
+    public class Quadras
+    {
+        public string NomeQuadra { get; set; }
+
+        public string Descricao { get; set; }
+
+    }
+}
